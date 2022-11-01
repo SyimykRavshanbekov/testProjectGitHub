49209ce7c08c3a79ee32e488f6db9da6be8888be
SyimykRavshanbekov <programmist00707@gmail.com>
2022-11-01 09:12:34 +0600

    commit random

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Random random = new Random();
+
+        int i = random.nextInt();
+
+        if (i>10){
+            System.out.println("Successfull");
+        }
     }
 }
